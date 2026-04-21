6a433f9bd394091edf60199b3bb2dca0914eb0a0
nakakota <nakakota@C010423442.zipangu.cac.co.jp>
2026-04-21 16:50:41 +0900

    1-5～4-4

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,56 @@
 
 package lesson01.challenge06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+		int sitstock = 30;
+		int syokstock = 30;
+		int pisstock = 30;
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+		System.out.println("シトロン      \\250 ・・・ 残り" + sitstock + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + syokstock + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + pisstock + "個\n");
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）\n");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン　　　>");
+		String sit = reader.readLine();
+		int sitko = Integer.parseInt(sit);
+		System.out.print("ショコラ　　　>");
+		String syok = reader.readLine();
+		int syokko = Integer.parseInt(syok);
+		System.out.print("ピスタージュ　>");
+		String pis = reader.readLine();
+		int pisko = Integer.parseInt(pis);
+
+		System.out.println("\nシトロン　　　>" + sit + '個');
+		System.out.println("ショコラ　　　>" + syok + '個');
+		System.out.println("ピスタージュ　>" + pis + "個\n");
+
+		System.out.println("合計個数　　" + (sitko + syokko + pisko) + "個");
+		System.out.println("合計金額　" + (sitko * 250 + syokko * 280 + pisko * 320) + "円");
+
+		System.out.println("\nをお買い上げですね。");
+		System.out.println("承りました。\n");
+
+		System.out.println("本日のおすすめ商品です。\n");
+
+		System.out.println("シトロン　　　\\250 ・・・ 残り" + (sitstock - sitko) + "個");
+		System.out.println("ショコラ　　　\\250 ・・・ 残り" + (syokstock - syokko) + "個");
+		System.out.println("ピスターシュ　\\250 ・・・ 残り" + (pisstock - pisko) + "個");
+
+	}
 
 }
--- Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
@@ -41,6 +41,66 @@
 
 package lesson01.challenge07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+		int sitstock = 30;
+		int syokstock = 30;
+		int pisstock = 30;
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+		System.out.println("シトロン      \\250 ・・・ 残り" + sitstock + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + syokstock + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + pisstock + "個\n");
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）\n");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン　　　>");
+		String sit = reader.readLine();
+		double sitko = Double.parseDouble(sit);
+		System.out.print("ショコラ　　　>");
+		String syok = reader.readLine();
+		double syokko = Double.parseDouble(syok);
+		System.out.print("ピスタージュ　>");
+		String pis = reader.readLine();
+		double pisko = Double.parseDouble(pis);
+
+		System.out.println("\nシトロン　　　>" + sitko + '個');
+		System.out.println("ショコラ　　　>" + syokko + '個');
+		System.out.println("ピスタージュ　>" + pisko + "個\n");
+
+		double totalcount = (sitko + syokko + pisko);
+		System.out.println("合計個数　　" + totalcount + "個");
+
+		double totalprice = (sitko * 250 + syokko * 280 + pisko * 320);
+		int totalprice2 = (int) totalprice;
+		System.out.println("合計金額　" + totalprice2 + "円");
+
+		System.out.println("\nをお買い上げですね。");
+		System.out.println("承りました。\n");
+
+		System.out.println("本日のおすすめ商品です。\n");
+
+		double lastsitstock = (sitstock - sitko);
+		int lastsitstock2 = (int) lastsitstock;
+		double lastsyokstock = (syokstock - syokko);
+		int lastsyokstock2 = (int) lastsyokstock;
+		double lastpisstock = (pisstock - pisko);
+		int lastpisstock2 = (int) lastpisstock;
+		System.out.println("シトロン　　　\\250 ・・・ 残り" + lastsitstock2 + "個");
+		System.out.println("ショコラ　　　\\250 ・・・ 残り" + lastsyokstock2 + "個");
+		System.out.println("ピスターシュ　\\250 ・・・ 残り" + lastpisstock2 + "個");
+
+	}
 
 }
